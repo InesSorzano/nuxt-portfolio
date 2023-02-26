e22d24f0406b316cb14ef3ef6d870dfd8f680b58
InesSorzano <inessorzano@gmail.com>
2023-02-26 12:38:02 +0000

    Deploying to gh-pages from @ InesSorzano/nuxt-portfolio@80644eebdd49b3330b6ed9b36be10154baebccae 🚀

---
+++ _nuxt/app.config.e2e9ede7.js
@@ -0,0 +1,1 @@
+import{v as n}from"./entry.81ecf2ec.js";const i={};n(i);
---
+++ _nuxt/index.6bcdc025.js
@@ -0,0 +1,1 @@
+import{_ as e,o as _,c}from"./entry.81ecf2ec.js";const n={};function t(o,a){return _(),c("h1",null," HOLI")}const r=e(n,[["render",t],["__scopeId","data-v-bf4ba0b3"]]);export{r as default};
