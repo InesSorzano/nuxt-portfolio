6382ca8cd602f0400358b8befb93a8e2bda9c0c7
Ines Sorzano <inessorzano@gmail.com>
2023-03-29 21:22:18 +0200

    add noopener and blank to links

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="flex ">
     <div class=" h-[calc(100vh-8rem)]  w-10 fixed left-16 justify-center flex flex-col items-center">
-      <a href="https://github.com/InesSorzano" >
+      <a href="https://github.com/InesSorzano" rel="noopener noreferrer" target="_blank">
          <img src="../assets/images/github.png" alt="github logo">
       </a>
-      <a href="https://www.linkedin.com/in/in%C3%A9s-sorzano-5a2907208" >
+      <a href="https://www.linkedin.com/in/in%C3%A9s-sorzano-5a2907208" rel="noopener noreferrer" target="_blank">
                   <img src="../assets/images/linkedin.png" alt="linkedin logo">
       </a>
     </div>
     <div class=" h-full text-center w-full mx-20 px-10 bg-slate-400 large-xl:mx-5 xl:px-1">
       <div id="index">
-        <img src="../assets/images/bg.jpg" alt="myself with pc" class="w-screen">
       </div>
      
       <p id="about">About me...</p>
