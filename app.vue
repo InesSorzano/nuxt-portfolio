c7df2cfc091cd174464ec4fc39877d35a38d7f7e
Ines Sorzano <inessorzano@gmail.com>
2023-05-31 19:26:09 +0200

    move imgs from assets to public

--- app.vue
+++ app.vue
@@ -5,10 +5,10 @@
     <!-- Redes sociales -->
     <div class="left-0 h-[calc(100vh-8rem)]  w-10 fixed md:left-16 justify-center flex flex-col items-center">
       <a href="https://github.com/InesSorzano" rel="noopener noreferrer" target="_blank">
-         <img src="~/assets/images/github.png" alt="github logo">
+         <img src="/images/github.png" alt="github logo">
       </a>
       <a href="https://www.linkedin.com/in/in%C3%A9s-sorzano-5a2907208" rel="noopener noreferrer" target="_blank">
-          <img src="~/assets/images/linkedin.png" alt="linkedin logo">
+          <img src="/images/linkedin.png" alt="linkedin logo">
       </a>
     </div>
     <!--  -->
@@ -26,7 +26,7 @@
           <!-- Imagen de perfil -->
           <div class=" m-10 h-56 lg:h-auto lg:w-56 flex bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"  title="Woman holding a mug">
             <div class="relative lg:w-48  m-auto h-100 mx-auto" >
-              <img src="~/assets/images/profile_pic.jpg" class="w-48  mx-auto rounded-full shadow-lg">
+              <img src="/images/profile_pic.jpg" class="w-48  mx-auto rounded-full shadow-lg">
             </div>
           </div>
   
@@ -46,10 +46,10 @@
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
               </p>
               <div class=" mt-4 space-x-3 md:mt-6">
-                  <a  href="files/cv.pdf" download="cv_inessorzano.pdf"  class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  ">
+                  <a  href="files/cv.pdf" download="cv_inessorzano.pdf"  class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300  ">
                     Currículum
                   </a>
-                  <a  class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                  <a  class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-500 ">
                     <NuxtLink :to="{ path: '/',hash:'#contact'}">Contact</NuxtLink>
                   </a>
               </div>
@@ -64,64 +64,71 @@
       <div id="experience" class="my-5">
         <h1 class="font-bold"> EXPERIENCE</h1>
         <CardBorder>
-          <div class="">
-            <div>
-              <a  href="https://www.panoimagen.com/"  rel="noopener noreferrer" target="_blank"><img src="~/assets/images/panoi.jpg" class="w-10 m-2 float-left relative"></a>
-              <p class="m-2 "> 2021 - <bold>Actualidad</bold></p>
-              <p><b style="color: green;"> Panoimagen </b> Desarrollo de aplicaciones web con Django </p>
-            </div>
-            <div class="">
-
-              <ul class=" pl-0">
-                <li>Bases de datos con PostgreSQL</li>
-                <li>Backend con Django (python)</li>
-                <li>Front end con HTML, CSS, Javascript, Node.js y Bootstrap</li>
-                <li>Despliegue con Nginx</li>
-                <li>Tareas en segundo plano con Redis y Celery</li>
-                <li>Detección de objetos con Detectron2 y Tensorflow lite</li>
-              </ul>
-            </div>
-            <br/>---
-            <div>
-              <img src="~/assets/images/studies.png" class="w-10 clear-both float-left">
-              <p class="mx-5 clear-both"> 2020 - 2021</p>
+          <Experience src="/images/panoi.jpg">
+            <template #content> 
+              <p class="float-right"> 2021 - Now</p>
+              <p><b style="color: green;"> Panoimagen </b></p>
+              <p>Web application development with Django and Nodejs </p>
+              <p>Backend: Django (Python)</p>
+              <p>Database management: PostgreSQL</p>
+              <p>Front end HTML, CSS, Javascript, Node.js, Bootstrap and TailwindCSS</p>
+              <p>Nginx</p>
+              <p>Tasks: Redis y Celery</p>
+              <p>Object detection: Detectron2 and Tensorflow lite</p>
+            </template>
+          </Experience>
+          
+          <Experience src="/images/studies.png">
+            <template #content> 
+              <p class="float-right"> 2020 - 2021</p>
+              <p><strong>  IES.Comercio</strong>  </p> 
+              <p>Grado Superior de Desarrollo de Aplicaciones Multiplataforma</p>
+              
               <p>Comercio Grado Superior de Desarrollo de Aplicaciones Multiplataforma</p>
-            </div>
-          </div>
-          
-          
+            </template>
+          </Experience>
         </CardBorder>
       </div>
       <!--  -->
 
       <!-- Contacto -->
-      <div id="contact" class="my-5">
-        <h1 class="font-bold"> CONTACT ME</h1>
-        <CardBorder>
-          <section class="py-6  mx-0">
-              <form ref="form" @submit.prevent="sendEmail" method="POST" target="_blank" rel="noopener noreferrer"  class="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
-                <p class="flex items-center mx-0 justify-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-2 sm:mr-6">
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                  </svg>
-                  <span class="mb-1">inessorzano@gmail.com</span>
-                </p>
-                <label class="block">
-                  <input type="text" placeholder="Name"  name="name" class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 ">
-                </label>
-                <label class="block">
-                  <input type="email" placeholder="Email" name="email" class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 ">
-                </label>
-                <label class="block">
-                  <textarea rows="3" v-model="message" name="message" class="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 "></textarea>
-                </label>
-                <button type="submit" id="send_mail"   class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300   self-center px-8 py-3 text-lg rounded ">Submit</button>
-              </form>
-        </section>
-      </CardBorder>
+      <a name ="contact">
+        <div id="contact" class="my-5">
+          <h1 class="font-bold"> CONTACT ME</h1>
+          <CardBorder>
+            <section class="py-6  mx-0">
+                <form ref="form" @submit.prevent="sendEmail" method="POST" target="_blank" rel="noopener noreferrer"  class="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
+                  <p class="flex items-center mx-0 justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-2 sm:mr-6">
+                      <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                      <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                    </svg>
+                    <a href="mailto:inessorzano@gmail.com" class="mb-1">inessorzano@gmail.com</a>
+                  </p>
+                  <label class="block">
+                    <input type="text" placeholder="Name"  name="name" id="name" class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-b-2 border-gray-200">
+                  </label>
+                  <label class="block">
+                    <input type="email" placeholder="Email" name="email" id="email" class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-b-2 border-gray-200 ">
+                  </label>
+                  <label class="block  ">
+                    <textarea rows="3" id="message" v-model="message" name="message" class="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-2 border-gray-200"></textarea>
+                  </label>
+                  <!-- HIDDEN SPINNER -->
 
-      </div>
+                  <button type="submit" id="send_mail"  class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-900 bg-border-2 border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300   self-center px-8 py-3 text-lg rounded ">
+                    <svg aria-hidden="true" role="status" class="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                    </svg>
+                    Submit
+                  </button>
+                </form>
+          </section>
+        </CardBorder>
+
+        </div>
+      </a>
       <!--  -->
     
     </div>
@@ -130,17 +137,17 @@
     <!-- Anclas derecha -->
     <div class=" h-[calc(100vh-8rem)]  w-10 fixed right-0 md:right-16 justify-center flex items-center ">
       <ul>
-        <li class="my-10 rounded-full border-solid border-2 border-black w-7  flex justify-center items-center">
-          <NuxtLink :to="{ path: '/',hash:'#index'}" class="items-center"><strong>.</strong></NuxtLink>
+        <li class="my-10 rounded-full border-2 border-black w-7  flex justify-center ">
+          <NuxtLink :to="{ path: '/',hash:'#index'}" class=""><strong>.</strong></NuxtLink>
         </li>
-        <li class="my-10 w-7  flex justify-center items-center">
+        <li class="my-10 w-7  flex justify-center ">
+          <NuxtLink :to="{ path: '/',hash:'#experience'}"><strong>.</strong></NuxtLink>
+        </li>
+        <li class="my-10 w-7  flex justify-center ">
           <NuxtLink :to="{ path: '/',hash:'#contact'}"><strong>.</strong></NuxtLink>
         </li>
-        <li class="my-10 w-7  flex justify-center items-center">
-          <NuxtLink :to="{ path: '/',hash:'#contact'}"><strong>.</strong></NuxtLink>
-        </li>
-        <li class="my-10 w-7  flex justify-center items-center">
-          <NuxtLink :to="{ path: '/',hash:'#contact'}"><strong>.</strong></NuxtLink>
+        <li class="my-10 w-7  flex justify-center ">
+          <NuxtLink :to="{ hash:'#contact'}"><strong>.</strong></NuxtLink>
         </li>
       </ul>
     </div>
@@ -160,10 +167,16 @@
       ]
     },
     methods: {
+      resetForm(){
+          document.getElementById("name").value="";
+          document.getElementById("email").value="";
+          document.getElementById("message").value="";
+      },
       sendEmail() {
         emailjs.sendForm('service_x653keh', 'template_3c4pmkm', this.$refs.form, 'YEznwqMeHyGnJg_Xx')
           .then((result) => {
               console.log('SUCCESS!', result.text);
+              this.resetForm();
           }, (error) => {
               console.log('FAILED...', error.text);
           });
---
+++ components/Experience.vue
@@ -0,0 +1,22 @@
+<template>
+    <br/>
+     <div id="">
+        <a  :href="href"  rel="noopener noreferrer" target="_blank"><img :src="src" class="w-10 m-2 float-left relative"></a>
+        <div class ="ml-28 text-left	">
+        <slot name="content"> Fallback Content </slot>
+
+        </div>
+
+    </div>
+    <br/>
+</template>
+
+<script >
+export default{
+    props: ["href", "src"]
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
