31af32bae17b21a5300a33c7e4f16e0fda2a4d99
Ines Sorzano <inessorzano@gmail.com>
2023-06-01 20:54:30 +0200

    app to index

--- components/Nav.vue
+++ components/Nav.vue
@@ -3,10 +3,9 @@
         <header class="shadow-sm bg-white ">
             <nav class="container mx-auto p-4 flex  justify-center">
                 <ul class="flex gap-4 ">
-                    <li> <NuxtLink :to="{ path: '/',hash:'#index'}">Home</NuxtLink></li>
-                    <li> <NuxtLink :to="{ path: '/',hash:'#about'}">About</NuxtLink></li>
-                    <li> <NuxtLink :to="{ path: '/',hash:'#experience'}">Experience</NuxtLink></li>
-                    <li> <NuxtLink :to="{ path: '/',hash:'#contact'}">Contact</NuxtLink></li>
+                    <li> <NuxtLink to="#index">Home</NuxtLink></li>
+                    <li> <NuxtLink to="#experience">Experience</NuxtLink></li>
+                    <li> <NuxtLink :to="{path: '/', hash: '#contact'}" v-scroll-to="{el: '#contact'}">Contact</NuxtLink></li>
                 </ul>
             </nav>
         </header>
