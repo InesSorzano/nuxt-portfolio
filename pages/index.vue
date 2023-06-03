aa5a8af287d045f62a2e9e147e28086595f6c0b1
Ines Sorzano <inessorzano@gmail.com>
2023-06-03 09:49:32 +0200

    contact inputs required and spinner onsubmit

--- pages/index.vue
+++ pages/index.vue
@@ -39,7 +39,7 @@
                 Inés Sorzano Cabrera
               </div>
               <span class="text-sm text-gray-500 ">
-                Full stack developer
+                Full stack junior developer
               </span>
 
               <p class="text-gray-700 text-base">
@@ -106,22 +106,22 @@
                     <a href="mailto:inessorzano@gmail.com" class="mb-1">inessorzano@gmail.com</a>
                   </p>
                   <label class="block">
-                    <input type="text" placeholder="Name"  name="name" id="name" class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-b-2 border-gray-200">
+                    <input type="text" placeholder="Name"  name="name" id="name" required class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-b-2 border-gray-200">
                   </label>
                   <label class="block">
-                    <input type="email" placeholder="Email" name="email" id="email" class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-b-2 border-gray-200 ">
+                    <input type="email" placeholder="Email" name="email" id="email" required class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-b-2 border-gray-200 ">
                   </label>
                   <label class="block  ">
-                    <textarea rows="3" id="message" v-model="message" name="message" class="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-2 border-gray-200"></textarea>
+                    <textarea rows="3" id="message" v-model="message" name="message" required class="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-2 border-gray-200"></textarea>
                   </label>
                   <!-- HIDDEN SPINNER -->
 
-                  <button type="submit" id="send_mail"  class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-900 bg-border-2 border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300   self-center px-8 py-3 text-lg rounded ">
-                    <svg aria-hidden="true" id="sending" role="status" class="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <button type="submit"   class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-900 bg-border-2 border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300   self-center px-8 py-3 text-lg rounded ">
+                    <svg aria-hidden="true" id="sending" role="status" class="hidden mx-auto inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
                       <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
                     </svg>
-                    Submit
+                    <p id="submit_text">Submit</p>
                   </button>
                 </form>
           </section>
@@ -140,7 +140,7 @@
         <li >
           <NuxtLink to="#index"  class="m-5 w-5 h-5 bg-slate-400 rounded-full flex justify-center"></NuxtLink>
         </li>
-        <li > 
+        <li >
           <NuxtLink to="#experience" class="m-5 w-5 h-5 bg-slate-900 rounded-full flex justify-center"> </NuxtLink>
         </li>
         <li>
@@ -170,25 +170,26 @@
           document.getElementById("email").value="";
           document.getElementById("message").value="";
       },
-      restoreSendButton(spinner,send_button){
+      restoreSendButton(spinner,submit_text){
         spinner.classList.add("hidden");
-        send_button.innerHTML = "Submit";
+        submit_text.innerHTML = "Submit";
       },
       sendEmail() {
         let spinner = document.getElementById("sending");
-        let send_button = document.getElementById("send_mail");
+        let submit_text = document.getElementById("submit_text");
         spinner.classList.remove("hidden");
-        send_button.innerHTML = "";
+        submit_text.innerHTML = "";
         emailjs.sendForm('service_x653keh', 'template_3c4pmkm', this.$refs.form, 'YEznwqMeHyGnJg_Xx')
           .then((result) => {
               console.log('SUCCESS!', result.text);
-              // this.restoreSendButton(spinner,send_button);
-              // this.resetForm();
+              this.restoreSendButton(spinner,submit_text);
+              this.resetForm();
           }, (error) => {
               console.log('FAILED...', error.text);
-              this.restoreSendButton(spinner,send_button);
+              this.restoreSendButton(spinner,submit_text);
           });
-      }
+      },
+      
     }
   }
 </script>
