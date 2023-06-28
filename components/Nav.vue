623fee5b58171bcc57666bc652893600b93106ce
Ines Sorzano <inessorzano@gmail.com>
2023-06-28 23:13:18 +0200

    change dark theme images

--- components/Nav.vue
+++ components/Nav.vue
@@ -3,7 +3,7 @@
         <header class="relative shadow-sm bg-slate-100 dark:shadow-gray-900 dark:bg-slate-800  dark:text-gray-100">
             
             <nav>
-                <button  @click="setColorTheme($colorMode.preference === 'dark' ? 'light' : 'dark')" class="right-3  top-3  h-8 w-8 z-10 absolute">
+                <button  @click="setColorTheme($colorMode.preference === 'dark' ? 'light' : 'dark')" class="right-3  top-3  h-8 w-8 z-10 absolute dark:bg-slate-100 rounded-full p-1" >
                 <img :src="'images/' + colorMode.preference + '.png'" >
                 </button>
                 <div  class="container mx-auto p-4 flex justify-center  ">
